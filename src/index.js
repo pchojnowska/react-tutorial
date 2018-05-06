d7348668a84f67a251ccb5b2b8b48174db0d7bc0
Paulina Chojnowska <antaria@vp.pl>
2018-05-06 17:49:44 +0200

    When someone wins, highlight the three squares that caused the win

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 
 function Square(props) {
     return (
-        <button className="square" onClick={props.onClick}>
+        <button className={props.winning ? "square winningSquare" : "square"} onClick={props.onClick}>
             {props.value}
         </button>
     );
@@ -19,6 +19,7 @@ class Board extends React.Component {
             <Square
                 value={this.props.squares[i]}
                 onClick={() => this.props.onClick(i)}
+                winning={didIWin(i, this.props.squares)}
             />
         );
     }
@@ -54,7 +55,7 @@ class Game extends React.Component {
                 squares: Array(9).fill(null)
             }],
             stepNumber: 0,
-            xIsNext: true,
+            xIsNext: true
         }
     }
 
@@ -130,6 +131,37 @@ class Game extends React.Component {
         );
     }
 }
+
+function checkWinningRow(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+    for (let i = 0; i<lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return lines[i];
+        }
+    }
+    return null
+}
+
+function didIWin(i, squares) {
+    const winningRow = checkWinningRow(squares);
+    if (winningRow) {
+        return winningRow.indexOf(i) > -1;
+    } else {
+        return false
+    }
+}
+
 
 function calculateWinner(squares) {
     const lines = [
